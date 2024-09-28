cb3a4ad68d48f9e53e1f34701014f3417122bb31
吟风抱月 <3472151649@qq.com>
2024-09-28 22:19:11 +0800

    初始化项目

---
+++ src/main/java/com/yygx/algostruct/binarySearch/BinarySearch.java
@@ -0,0 +1,13 @@
+package com.yygx.algostruct.binarySearch;
+
+/**
+ * <p>
+ * 注释：二分查找
+ * </p>
+ * Since: 2024/9/28 <p>
+ * Author: <a href="https://github.com/YFBY-Java" target="_blank">吟风抱月</a>
+ */
+public class BinarySearch {
+
+
+}
