4eba7bc7ecfa488ddfe1601e2f2375949808a723
吟风抱月 <3472151649@qq.com>
2024-10-11 22:19:59 +0800

    【feat】二分查找 LeetCode34优化

--- src/main/java/com/yygx/algostruct/binarySearch/LeetCode34.java
+++ src/main/java/com/yygx/algostruct/binarySearch/LeetCode34.java
@@ -2,56 +2,51 @@ package com.yygx.algostruct.binarySearch;
 
 import java.util.Arrays;
 
-/**
- * @version 1.0
- * @Copyright (c) 合思技术团队 https://www.hosecloud.com/
- * @authur：zhaojiawei zhaojiawei@hosecloud.com
- * @Package com.yygx.algostruct.binarySearch
- * @Project：AlgoStruct
- * @date 2024/10/10 下午10:59
- */
+
 public class LeetCode34 {
     public static int[] searchRange(int[] nums, int target) {
-        int[] result = new int[2];
+        int left = searchLeft(nums,target);
+        if(left == -1){
+            return new int[]{-1,-1};
+        }else{
+            return new int[]{left,searchRight(nums,target)};
+        }
 
-        result[0] = searchFirst(nums, target);
-        result[1] = searchLast(nums,target);
-        return result;
     }
 
-    public static int searchFirst(int[] nums, int target){
+
+    public static int searchLeft(int[] nums, int target){
         int i = 0,j = nums.length-1;
-        int candidate = -1;
+        int cadidate = -1;
         while(i<=j){
             int middle = (i+j)>>>1;
-            if(target<nums[middle]){
+            if(target < nums[middle]){
                 j = middle-1;
-            }else if(nums[middle] < target){
+            }else if(nums[middle]<target){
                 i = middle+1;
             }else{
-                candidate = middle;
-                j = middle - 1;
+                cadidate = middle;
+                j = middle-1;
             }
         }
-        return candidate;
+        return cadidate;
     }
 
-
-    public static int searchLast(int[] nums, int target){
+    public static int searchRight(int[] nums, int target){
         int i = 0,j = nums.length-1;
-        int candidate = -1;
-        while (i<=j){
+        int cadidate = -1;
+        while(i<=j){
             int middle = (i+j)>>>1;
-            if(target<nums[middle]){
+            if(target < nums[middle]){
                 j = middle-1;
-            }else if(nums[middle] < target){
-                i = middle +1;
+            }else if(nums[middle]<target){
+                i = middle+1;
             }else{
-                candidate = middle;
-                i = middle + 1;
+                cadidate = middle;
+                i = middle+1;
             }
         }
-        return candidate;
+        return cadidate;
     }
 
 
@@ -61,5 +56,9 @@ public class LeetCode34 {
 
         int[] ints = searchRange(nums, target);
         System.out.println(Arrays.toString(ints));
+        int[] nums2 = {};
+        int target2 = 0;
+        int[] ints2 = searchRange(nums2, target2);
+        System.out.println(Arrays.toString(ints2));
     }
 }
