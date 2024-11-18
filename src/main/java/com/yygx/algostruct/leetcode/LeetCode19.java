2c9d9c6b86ac08ed4503608e6a59cf20fb90c5b1
吟风抱月 <3472151649@qq.com>
2024-11-18 11:06:35 +0800

    【feat】LeetCode.19删除链表的倒数第n个节点

---
+++ src/main/java/com/yygx/algostruct/leetcode/LeetCode19.java
@@ -0,0 +1,110 @@
+package com.yygx.algostruct.leetcode;
+
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * LeetCode.19删除链表的倒数第n个节点
+ * <p>
+ *     给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ * </p>
+ */
+public class LeetCode19 {
+
+
+    /**
+     * 计算链表长度
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode node = head;
+        ListNode quick = node;
+        ListNode slow = node;
+        int size = 0;
+        while (quick != null){
+            quick = quick.next;
+            size++;
+        }
+        int index = size - n - 1;
+        if(index < 0) return head.next;
+        for (int i = 0; i < index; i++) {
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return node;
+    }
+
+
+    /**
+     * 栈
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEndStack(ListNode head, int n) {
+        ListNode dummy = new ListNode(0,head);
+        Deque<ListNode> stack = new LinkedList<ListNode>();
+        ListNode current = dummy;
+        while (current != null){
+            stack.push(current);
+            current = current.next;
+        }
+        for (int i = 0; i < n; i++) {
+            stack.pop();
+        }
+        ListNode pre = stack.peek();
+        pre.next = pre.next.next;
+        return dummy.next;
+    }
+
+
+    /**
+     * 双指针
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEndPointTwo(ListNode head, int n) {
+        ListNode quick = head;
+        ListNode dummy = new ListNode(0, head);
+        ListNode slow = dummy;
+        for (int i = 0; i < n; i++) {
+            quick = quick.next;
+        }
+        while (quick != null){
+            quick = quick.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+        ListNode node7 = new ListNode(7);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+
+        ListNode node = new LeetCode19().removeNthFromEndPointTwo(node1, 1);
+        while (node != null){
+            System.out.println(node.val);
+            node = node.next;
+        }
+
+    }
+
+}
