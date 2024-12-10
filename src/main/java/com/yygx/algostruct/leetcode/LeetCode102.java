cc7d11edbe20d231f29b1d2dd891501b2447bf48
吟风抱月 <3472151649@qq.com>
2024-12-11 00:15:22 +0800

    【feat】LeetCode102.二叉树的层序遍历

---
+++ src/main/java/com/yygx/algostruct/leetcode/LeetCode102.java
@@ -0,0 +1,67 @@
+package com.yygx.algostruct.leetcode;
+
+
+import com.yygx.algostruct.datastructure.queue.ArrayQueue3;
+
+import java.util.*;
+
+/**
+ * LeetCode102.二叉树的层序遍历
+ * <p>
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+ * </p>
+ */
+public class LeetCode102 {
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * 使用队列层序遍历二叉树
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            List<Integer> level = new ArrayList<>();
+            int currentLevelSize = queue.size();
+            for (int i = 0; i < currentLevelSize; i++) {
+                TreeNode node = queue.poll();
+                if (node != null) {
+                    level.add(node.val);
+                    if(node.left != null){
+                        queue.offer(node.left);
+                    }
+                    if(node.right != null){
+                        queue.offer(node.right);
+                    }
+                }
+            }
+            result.add(level);
+        }
+        return result;
+    }
+}
