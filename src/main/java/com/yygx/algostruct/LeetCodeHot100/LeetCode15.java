f10b578f815bdcd3bd0fecc61481a8e103576385
吟风抱月 <3472151649@qq.com>
2025-07-28 16:08:55 +0800

    LeetCode15. 三数之和

--- src/main/java/com/yygx/algostruct/LeetCodeHot100/LeetCode15.java
+++ src/main/java/com/yygx/algostruct/LeetCodeHot100/LeetCode15.java
@@ -154,10 +154,8 @@ public class LeetCode15 {
         // 排序
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 2; i++) {
-            if (nums[i] > 0)
-                break;
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             int left = i + 1;
             int right = nums.length - 1;
             while (left < right) {
@@ -194,6 +192,38 @@ public class LeetCode15 {
                 int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {  // 加入到结果集，并让左右指针跳过此组合的元素
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                }
+                if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+
+
+    // 双指针
+    public List<List<Integer>> threeSumDualPointers3(int[] nums) {
+        // 定义一个结果集
+        List<List<Integer>> result = new ArrayList<>();
+        // 排序
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    // 跳过重复的元素组合
                     while (left < right && nums[left] == nums[left + 1]) left++;
                     while (left < right && nums[right] == nums[right - 1]) right--;
                 }
