9c11f2895a2d9d602b48ea67168435bcd7a65a98
吟风抱月 <3472151649@qq.com>
2025-09-02 22:29:27 +0800

    LeetCode739. 每日温度

---
+++ src/main/java/com/yygx/algostruct/leetcode/LeetCode739.java
@@ -0,0 +1,48 @@
+package com.yygx.algostruct.leetcode;
+
+
+import java.util.Stack;
+
+/**
+ * LeetCode739. 每日温度
+ * 给定一个整数数组 temperatures，表示每天的温度，返回一个数组 answer，<br/>
+ * 其中 answer[i] 是指对于第 i 天，下一个更高温度出现在几天后。<br/>
+ * 如果气温在这之后都不会升高，请在该位置用 0 来代替。<br/>
+ * <br/>
+ * 示例 1：<br/>
+ * 输入: temperatures = [73,74,75,71,69,72,76,73]<br/>
+ * 输出: [1,1,4,2,1,1,0,0]<br/>
+ * <br/>
+ * 示例 2：<br/>
+ * 输入: temperatures = [30,40,50,60]<br/>
+ * 输出: [1,1,1,0]<br/>
+ * <br/>
+ * 示例 3：<br/>
+ * 输入: temperatures = [30,60,90]<br/>
+ * 输出: [1,1,0]<br/>
+ */
+
+public class LeetCode739 {
+
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        // 单调栈,不大于的元素都入栈，大于的出栈
+        // 栈存储还没有找到更高温度的 元素 的索引
+        // 定义返回结果
+        int[] answer = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            // 获取当前元素
+            int current = temperatures[i];
+            // 栈中有元素，且当前元素大于栈顶元素
+            while (!stack.isEmpty() && current > temperatures[stack.peek()]){
+                Integer pop = stack.pop();
+                answer[pop] = i - pop;
+            }
+            stack.push(i); // 把当前元素索引放入栈顶
+        }
+        return answer;
+    }
+
+
+}
