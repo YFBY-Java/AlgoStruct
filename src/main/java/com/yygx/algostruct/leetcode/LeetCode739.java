ce050994898586630eda49c8b41975197c127b97
吟风抱月 <3472151649@qq.com>
2025-09-03 21:47:25 +0800

    LeetCode739. 每日温度

--- src/main/java/com/yygx/algostruct/leetcode/LeetCode739.java
+++ src/main/java/com/yygx/algostruct/leetcode/LeetCode739.java
@@ -35,11 +35,32 @@ public class LeetCode739 {
             // 获取当前元素
             int current = temperatures[i];
             // 栈中有元素，且当前元素大于栈顶元素
-            while (!stack.isEmpty() && current > temperatures[stack.peek()]){
+            while (!stack.isEmpty() && current > temperatures[stack.peek()]) {
                 Integer pop = stack.pop();
                 answer[pop] = i - pop;
             }
             stack.push(i); // 把当前元素索引放入栈顶
+        }
+        return answer;
+    }
+
+
+    public int[] dailyTemperaturesTest(int[] temperatures) {
+        // 单调栈，不比当前温度高的入栈，遇到比当前温度高的出栈顶，修改对应元素
+        // 返回结果
+        int[] answer = new int[temperatures.length];
+        // 定义栈
+        Stack<Integer> stack = new Stack<>();  // 栈存储的是 上一个非严格递增温度 的索引
+        for (int i = 0; i < temperatures.length; i++) {
+
+            int current = temperatures[i];
+            // 栈不为空，并且当前温度大于 栈顶索引指向的温度
+            while (!stack.isEmpty() && current > temperatures[stack.peek()]){
+                // 栈顶索引 对应的 answer 元素
+                Integer pop = stack.pop();
+                answer[pop] = i - pop;
+            }
+            stack.push(i);   // 当前温度入栈
         }
         return answer;
     }
