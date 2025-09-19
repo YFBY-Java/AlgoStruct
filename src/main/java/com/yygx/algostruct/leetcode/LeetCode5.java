ec4254329efb5a5597d5af588e92703b94f71fcb
吟风抱月 <3472151649@qq.com>
2025-09-19 22:38:33 +0800

    LeetCode5

--- src/main/java/com/yygx/algostruct/leetcode/LeetCode5.java
+++ src/main/java/com/yygx/algostruct/leetcode/LeetCode5.java
@@ -22,37 +22,66 @@ package com.yygx.algostruct.leetcode;
 public class LeetCode5 {
 
 
+//    // 中心扩展法
+//    public String longestPalindrome(String s) {
+//        if(s == null || s.length() < 2) return s;
+//        //  记录最长回文串的长度 和 起始索引
+//        int max = 0;
+//        int start = 0;
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            // 奇数回文串
+//            int odd = centerExpansion(s, i, i);
+//            // 偶数回文串
+//            int even = centerExpansion(s, i, i + 1);
+//            int currentMax = Math.max(odd,even);
+//            if(currentMax > max){
+//                max = currentMax;
+//                start = i - (max - 1) / 2;
+//            }
+//        }
+//        return s.substring(start,start + max);
+//    }
+//
+//    // 返回的是回文串的长度
+//    public int centerExpansion(String s,int left,int right){
+//        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+//            left--;
+//            right++;
+//        }
+//        return right -  left - 1;
+//    }
+
 
     // 中心扩展法
     public String longestPalindrome(String s) {
-        if(s == null || s.length() < 2) return s;
-        //  记录最长回文串的长度 和 起始索引
-        int max = 0;
-        int start = 0;
+        if (s == null || s.length() == 1) return s;
+        int max = 0; // 最长回文串长度
+        int start = 0;  // 最长回文串的开始位置
 
         for (int i = 0; i < s.length(); i++) {
-            // 奇数回文串
+            // 奇数
             int odd = centerExpansion(s, i, i);
-            // 偶数回文串
+            // 偶数
             int even = centerExpansion(s, i, i + 1);
-            int currentMax = Math.max(odd,even);
-            if(currentMax > max){
-                max = currentMax;
-                start = i - (max - 1) / 2;
+            int current = Math.max(odd, even);
+            if (current > max) {
+                max = current;
+                start = i - (current - 1) / 2;
             }
         }
-        return s.substring(start,start + max);
+        return s.substring(start, start + max);
     }
 
-    // 返回的是回文串的长度
-    public int centerExpansion(String s,int left,int right){
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+
+    // 中心扩展  返回值是长度
+    public int centerExpansion(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
-        return right -  left - 1;
+        return right - left - 1;
     }
-
 
 
 }
