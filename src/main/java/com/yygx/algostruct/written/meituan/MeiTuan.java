0f771c4efacfa3ea2ca6e0bf18d2df7a09dff5ad
吟风抱月 <3472151649@qq.com>
2025-10-11 00:34:44 +0800

    test

---
+++ src/main/java/com/yygx/algostruct/written/meituan/MeiTuan.java
@@ -0,0 +1,6 @@
+package com.yygx.algostruct.written.meituan;
+
+
+// 美团笔试
+public class MeiTuan {
+}
---
+++ src/main/java/com/yygx/algostruct/written/xiaomi/XiaoMi.java
@@ -0,0 +1,6 @@
+package com.yygx.algostruct.written.xiaomi;
+
+
+// 小米笔试-Java
+public class XiaoMi {
+}
